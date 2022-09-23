483db7ccfd1ee17107f81247c87c045a0c1b6219
gochigochi <diegoeliseoiovane@gmail.com>
2022-09-23 12:43:57 -0300

    change api

--- pages/index.js
+++ pages/index.js
@@ -10,22 +10,22 @@ import axios from 'axios'
 export default function Home(props) {
 
   // const [ expos, setExpos ] = useState([])
-  // const url = 'https://dummyapi.io/data/v1/post?limit=10'
+  // const url = 'https://hub.dummyapis.com/ImagesList?text=Test&noofimages=10&height=120&width=120'
 
 
   // useEffect(() => {
-  //   fetch(url)
-  //   .then(res => console.log(res))
+  //   fetch(url, {appId: '0JyYiOQXQQr5H9OEn21312'})
+  //   .then(res => console.log(res.json()))
   // }, [])
 
 
-
+  
   const [ products, setProducts ] = useState([])
 
   useEffect(() => {
 
     const api = new WooCommerceRestApi({
-      url: "http://rent-internet.com",
+      url: "https://rent-internet.com",
       consumerKey: "ck_80a2664a198dce072225a7850dcf185e53bbd77a",
       consumerSecret: "cs_9abd63dc1ede4fda0e2ef4182a46e6a2023a91df",
       version: "wc/v3"
