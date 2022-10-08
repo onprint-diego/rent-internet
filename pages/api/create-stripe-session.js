24d8b61dbf416ab23499a2e30584c9458da9d335
gochigochi <diegoeliseoiovane@gmail.com>
2022-10-08 17:33:41 -0300

    remove 'next export' from pckg.json - solve 404 issue on check9out

--- pages/api/create-stripe-session.js
+++ pages/api/create-stripe-session.js
@@ -2,23 +2,73 @@ import { api } from "../../utils/wocommerce";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 async function CreateStripeSession(req, res) {
+    const { item } = req.body;
+
+    const cart = item
+    const response = await api.get("products")
+    const modemName = response.data[4].name
+    const modemPrice = parseInt(response.data[4].price)
+    const powerBankPrice = parseInt(response.data[0].price)
+    const adapterPrice = parseInt(response.data[1].price)
+    const shippingFee = parseInt(response.data[4].attributes[0].options[0])
+    const subTotal = modemPrice * item.qty
+
+    const items = []
+    items.push({
+        price_data: {
+            currency: 'usd',
+            product_data: {
+                name: modemName,
+            },
+            unit_amount: subTotal * 100,
+        },
+        quantity: 1,
+    })
+
+    items.push({
+        price_data: {
+            currency: 'usd',
+            product_data: {
+                name: "Shipping Fee",
+            },
+            unit_amount: shippingFee * 100,
+        },
+        quantity: 1,
+    })
+
+    if(cart.adapter.is) {
+        items.push({
+            price_data: {
+                currency: 'usd',
+                product_data: {
+                    name: "Adaptador",
+                },
+                unit_amount: adapterPrice * 100,
+            },
+            quantity: 1,
+        })
+    }
+
+    if(cart.powerBank.is) {
+        items.push({
+            price_data: {
+                currency: 'usd',
+                product_data: {
+                    name: "Power Bank",
+                },
+                unit_amount: powerBankPrice * 100,
+            },
+            quantity: 1,
+        })
+    }
 
     const session = await stripe.checkout.sessions.create({
+        // success_url: 'http://localhost:3000',
+        // cancel_url: 'http://localhost:3000',
         success_url: 'https://rent-internet/succesful',
         cancel_url: 'https://rent-internet/cancel',
         mode: 'payment',
-        line_items: [
-            {
-                price_data: {
-                    currency: 'usd',
-                    product_data: {
-                        name: "Modem",
-                    },
-                    unit_amount: 200 * 100,
-                },
-                quantity: 1,
-            }
-        ],
+        line_items: items,
     });
 
     res.json({ id: session.id });
