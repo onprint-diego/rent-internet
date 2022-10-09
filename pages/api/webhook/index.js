ad925cca9f1e6fab6b8c33824a36bbd56fa14e6a
gochigochi <diegoeliseoiovane@gmail.com>
2022-10-08 23:56:19 -0300

    implementing webhook

--- pages/api/webhook/index.js
+++ pages/api/webhook/index.js
@@ -12,22 +12,22 @@ export const config = {
 }
 
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        const requestBuffer = await buffer(req)
-        const payload = requestBuffer.toString()
-        const sig = req.headers["stripe-signature"]
+    // if (req.method === 'POST') {
+    //     const requestBuffer = await buffer(req)
+    //     const payload = requestBuffer.toString()
+    //     const sig = req.headers["stripe-signature"]
 
-        let event
+    //     let event
 
-        try {
-            event = stripe.webhooks.constructEvent(payload, sig, endpointSecret)
-        } catch(err) {
-            console.log('Error', err.message)
-            return res.status(400).send(`Webhook error: ${err.message}`)
-        }
+    //     try {
+    //         event = stripe.webhooks.constructEvent(payload, sig, endpointSecret)
+    //     } catch(err) {
+    //         console.log('Error', err.message)
+    //         return res.status(400).send(`Webhook error: ${err.message}`)
+    //     }
 
-        if(event.type === 'checkout.session.completed') {
-            const session = event.data.object
-        }
-    }
+    //     if(event.type === 'checkout.session.completed') {
+    //         const session = event.data.object
+    //     }
+    // }
 }
