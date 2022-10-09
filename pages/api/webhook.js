0bbf1f0adc024f37fbe4dcd75400ceb56171fd09
gochigochi <diegoeliseoiovane@gmail.com>
2022-10-09 17:00:23 -0300

    test webhook

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -19,6 +19,8 @@ export default async function handler(req, res) {
         const payload = reqBuffer.toString()
         const sig = req.headers["stripe-signature"]
 
+        console.log(req.body, req, payload)
+
         let event
 
         try {
