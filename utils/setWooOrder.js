c7a9793260ab93c707943892c4ff15cc84e02e1a
gochigochi <diegoeliseoiovane@gmail.com>
2022-10-12 22:51:34 -0300

    Reduce Api response time separating woo functions

--- utils/setWooOrder.js
+++ utils/setWooOrder.js
@@ -1,6 +1,6 @@
-import { api } from '../../utils/wocommerce'
+import { api } from './wocommerce'
 
-export const setWooOrder = ( session ) => {
+export const setWooOrder = (session) => {
 
     //Format the incoming detail from Stripe line items to Woo orders format
     // const lineItems = products.map(product => {
@@ -10,48 +10,48 @@ export const setWooOrder = ( session ) => {
     //         price: product.price.unit_amount / 100,
     //     }
     // })
-    
+
     const data = {
         id: session.id,
         payment_method: "Card",
         payment_method_title: "Card",
         set_paid: true,
         billing: {
-          first_name: session.metadata.customerName,
-          last_name: "Doe",
-          address_1: "969 Market",
-          address_2: "",
-          city: "San Francisco",
-          state: "CA",
-          postcode: "94103",
-          country: "US",
-          email: session.metadata.customerEmail,
-          phone: "(555) 555-5555"
+            first_name: session.metadata.customerName,
+            last_name: "Doe",
+            address_1: "969 Market",
+            address_2: "",
+            city: "San Francisco",
+            state: "CA",
+            postcode: "94103",
+            country: "US",
+            email: session.metadata.customerEmail,
+            phone: "(555) 555-5555"
         },
         shipping: {
-          first_name: "John",
-          last_name: "Doe",
-          address_1: "969 Market",
-          address_2: "",
-          city: "San Francisco",
-          state: "CA",
-          postcode: "94103",
-          country: "US"
+            first_name: "John",
+            last_name: "Doe",
+            address_1: "969 Market",
+            address_2: "",
+            city: "San Francisco",
+            state: "CA",
+            postcode: "94103",
+            country: "US"
         },
         line_items: [
-          {
-            name: 'Item',
-            quantity: 2,
-            price: 10,
-          },
+            {
+                name: 'Item',
+                quantity: 2,
+                price: 10,
+            },
         ],
-      };
-      
-      api.post("orders", data)
+    };
+
+    api.post("orders", data)
         .then((response) => {
-          console.log(response.data);
+            console.log(response.data);
         })
         .catch((error) => {
-          console.log(error.response.data);
-        });     
+            console.log(error.response.data);
+        });
 }
