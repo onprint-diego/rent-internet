36f8d3d6fe5db1fafa780d0841ea4f1fa900e41e
gochigochi <diegoeliseoiovane@gmail.com>
2022-11-04 15:02:47 -0300

    test transfer mailing

--- pages/api/webhook-recharge.js
+++ pages/api/webhook-recharge.js
@@ -81,67 +81,43 @@ export default async function handler(req, res) {
         res.json({ message: 'Error setting woo order' })
       }
 
-      // SEND MAIL
-      // const formatedItemsHtml = itemsList.data.map(item => {
-      //   return `<p>${item.description} x${item.quantity} - ${item.amount_total / 100}</p>`
-      // }).join('<br>')
-
-      // const msg = {
-      //   to: session.customer_details.email,
-      //   from: 'rent@rent-internet.com',
-      //   subject: 'Confirmación de reserva de módem Rent Internet',
-      //   html: `
-      //     <h1>Gracias por la reserva</h1>
-      //     <h4>Número de órden: ${wooOrderId.data.id}</h4>
-      //     ${formatedItemsHtml}
-      //     `,
-      // };
-
       const html = `
-      <div style="width: 75%;background-color: #f7f7f7;padding-bottom: 10rem;margin: 0 auto;border-radius: 4px; overflow: hidden;">
-        <div style="background-color: #ffffff;width: 90%;max-width: 550px;margin: 0 auto;border-radius: 3px;border;1px solid #e0e0e0;overflow: hidden;">
-        <div style="background-color: #1966d1;padding: 2rem;color: #ffffff;">
-            <h1>Gracias por su reserva</h1>
-        </div>
-        <div style="padding: 2rem;">
-            <p>Hemos recibido tu orden de reserva. El número de orden es:</p>
-            <p style="text-align: center;font-size: 4rem;font-weight: bold;margin: 2rem 0;color: #1966d1;">${content.id}</p>
-            <table style="border-collapse: collapse;border-spacing: 0;margin-top: 1rem;width: 95%;border-radius: 4px;">
-                <thead style="background-color: #dfdfe2;">
-                    <tr>
-                        <th style="border: 1px solid #dfdfe2;text-align: left;padding: 1rem;">Producto</th>
-                        <th style="border: 1px solid #dfdfe2;text-align: left;padding: 1rem;">Cantidad</th>
-                        <th style="border: 1px solid #dfdfe2;text-align: left;padding: 1rem;">Precio</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td style="border: 1px solid #dfdfe2;text-align: left;padding: 1rem;">${content.line_items[0].name}</td>
-                        <td style="border: 1px solid #dfdfe2;text-align: left;padding: 1rem;">${content.line_items[0].quantity}</td>
-                        <td style="border: 1px solid #dfdfe2;text-align: left;padding: 1rem;">USD ${content.line_items[0].total}</td>
-                    </tr>
-                    <tr>
-                        <td style="border: 1px solid #dfdfe2;text-align: left;padding: 1rem;" colspan="2">Total</td>
-                        <td style="border: 1px solid #dfdfe2;text-align: left;padding: 1rem;">USD ${content.total}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <div style="margin-top: 2rem;">
-                <p style="font-weight: bold;">Método de pago</p>
-                <p>${content.payment_method}</p>
+          <div style="width: 75%;background-color: #f7f7f7;padding-bottom: 10rem;margin: 0 auto;border-radius: 4px; overflow: hidden;">
+              <div style="background-color: #ffffff;width: 90%;max-width: 550px;margin: 0 auto;border-radius: 3px;border;1px solid #e0e0e0;overflow: hidden;">
+              <div style="background-color: #1966d1;padding: 2rem;color: #ffffff;">
+                  <h1>Gracias por su reserva</h1>
+              </div>
+              <div style="padding: 2rem;">
+                  <p>Hemos recibido tu orden de reserva. El número de orden es:</p>
+                  <p style="text-align: center;font-size: 4rem;font-weight: bold;margin: 2rem 0;color: #1966d1;">${wooOrderId.data.id}</p>
+                  <table style="border-collapse: collapse;border-spacing: 0;margin-top: 1rem;width: 95%;border-radius: 4px;">
+                      <thead style="background-color: #dfdfe2;">
+                          <tr>
+                              <th style="border: 1px solid #dfdfe2;text-align: left;padding: 1rem;">Producto</th>
+                              <th style="border: 1px solid #dfdfe2;text-align: left;padding: 1rem;">Cantidad</th>
+                              <th style="border: 1px solid #dfdfe2;text-align: left;padding: 1rem;">Precio</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          <tr>
+                              <td style="border: 1px solid #dfdfe2;text-align: left;padding: 1rem;" colspan="2">Total</td>
+                              <td style="border: 1px solid #dfdfe2;text-align: left;padding: 1rem;">USD </td>
+                          </tr>
+                      </tbody>
+                  </table>
+                  <div style="margin-top: 2rem;">
+                      <p style="font-weight: bold;">Método de pago</p>
+                      <p>${completedOrder.payment_method}</p>
+                  </div>
+              </div>
             </div>
         </div>
-        <div id="footer>
-        </div>
-        </div>
-    </div>
-`
+      `
 
       const msg = {
         to: content.billing.email,
         from: email,
         subject,
-        name: name,
         html: html,
       };
 
